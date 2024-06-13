7457053a50776a8ba493abdd22b314f4d825b30f
irostub <irostub@gmail.com>
2024-06-13 14:40:19 +0900

    [add] finish

--- caller/src/main/java/io/whatap/caller/controller/MainController.java
+++ caller/src/main/java/io/whatap/caller/controller/MainController.java
@@ -1,12 +1,47 @@
 package io.whatap.caller.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
 @Controller
 public class MainController {
+    private final RestTemplate restTemplate;
+    @Value("${callee.url}")
+    private String calleeUrl;
+
     @GetMapping
     public String index() {
         return "index";
     }
+
+    @GetMapping("/health")
+    public String health() {
+        String result = restTemplate.getForObject(calleeUrl + "/callee/health", String.class);
+        return "index";
+    }
+    @GetMapping("/log")
+    public String createLog() {
+        String result = restTemplate.postForObject(calleeUrl + "/callee/log", null, String.class);
+        return "index";
+    }
+    @GetMapping("/calculate/all")
+    public String cpuLoad() {
+        String result = restTemplate.getForObject(calleeUrl + "/callee/calculate/all", String.class);
+        return "index";
+    }
+    @GetMapping("/db/postgresql/sleep")
+    public String dbSleep() {
+        restTemplate.getForObject(calleeUrl + "/callee/db/postgresql/sleep", String.class);
+        return "index";
+    }
+
+
+
+
+
+
 }
