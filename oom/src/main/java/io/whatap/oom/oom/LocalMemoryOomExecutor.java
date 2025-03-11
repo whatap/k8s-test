fe968b01bf39e7e063e8993b99fd4cfc7b545ae9
irostub <shinyushinnya@gmail.com>
2025-03-11 16:10:52 +0900

    Fix option

--- oom/src/main/java/io/whatap/oom/oom/LocalMemoryOomExecutor.java
+++ oom/src/main/java/io/whatap/oom/oom/LocalMemoryOomExecutor.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +16,9 @@ import java.util.List;
 @ConditionalOnProperty(name = "app.memory.type", havingValue = "local")
 @Component
 public class LocalMemoryOomExecutor implements OutOfMemoryExecutor{
+    @Value("${app.memory.limit}")
+    private Integer limitMemory;
+
     @Value("${app.memory.input-huge-memory}")
     private Integer hugeMemory;
 
@@ -23,8 +29,15 @@ public class LocalMemoryOomExecutor implements OutOfMemoryExecutor{
 
     @Override
     public void execute() {
-        List<byte[]> bytes = new ArrayList<>();
-        bytes.add(new byte[1024*1024*hugeMemory]);
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
+
+        long used = heapMemoryUsage.getUsed();       // 사용 중인 힙 메모리 (bytes)
+
+        if(used < limitMemory * 1024 * 1024){
+            List<byte[]> bytes = new ArrayList<>();
+            bytes.add(new byte[1024*1024*hugeMemory]);
+        }
     }
 
     @Override
